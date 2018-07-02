dab9f10cc48c0eb974b671035848bafb75598bd1
Tao <werewaswas@live.com>
2018-07-02 06:38:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,12 +7,13 @@ class Dog
   attr_accessor :name
 
   def initialize(name)
+    @name = name
     @@all < self
   end
 
   def self.all
     @@all.each do |dog|
-      puts dog
+      puts dog.name
     end
 
   end
